5d4e806e1c12093c7a4a8300208dd380fa4c78f5
ilanhk <ilanlieberman@hotmail.com>
2022-01-31 15:53:53 +0800

    testing complete for new laptop

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -6,7 +6,7 @@ import { startLogin } from '../actions/auth';
 export const LoginPage = ({ startLogin }) =>(
     <div className='box-layout'>
         <div className='box-layout__box'>
-            <h1 className='box-layout__title'>Expensify App</h1>
+            <h1 className='box-layout__title'>Expensify</h1>
             <p>Time to organize and control your expenses</p>
             <button className='button' onClick={startLogin}>Login with Google</button>
         </div>
